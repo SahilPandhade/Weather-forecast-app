936fae1e332e8ce83b68ef2f9a254d5407ff7e43
SahilPandhade <sahilpandhade@rediffmail.com>
2023-09-02 20:35:09 +0530

    Added conditional rendering to Globe component

--- src/components/Globe/Globe.tsx
+++ src/components/Globe/Globe.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from "react"
 import Earth from './components/Earth/Earth';
 import { useWeatherData } from '../../hooks/useWeatherData';
+import Loader from '../Loader/Loader';
 const Globe: React.FC<{ cityName: string; countryCode: string }> = ({ cityName, countryCode }) => {
-    const {data} = useWeatherData(cityName,countryCode)
+    const { data } = useWeatherData(cityName, countryCode)
+    const [texturesLoaded, setTexturesLoaded] = useState(false);
+    const handleGlobeLoaded = () => {
+        setTexturesLoaded(true)
+        console.log(texturesLoaded)
+    }
     return (
         <div className="globe-container">
-            <Canvas>
+            {!texturesLoaded && (<Loader />) }
+           <Canvas>
                 <Suspense fallback={null}>
-                    <Earth latitude={data?.coord?.lat ?? 0} longitude={data?.coord?.lon ?? 0}/>
+                    <Earth latitude={data?.coord?.lat ?? 0} longitude={data?.coord?.lon ?? 0} handleGlobeLoaded={handleGlobeLoaded} />
                 </Suspense>
             </Canvas>
         </div>
--- src/components/Globe/components/Earth/Earth.tsx
+++ src/components/Globe/components/Earth/Earth.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import {  Stars } from '@react-three/drei'
+import { Stars } from '@react-three/drei'
 import * as THREE from 'three'
 import EarthDayMap from '../../../../assets/8k_earth_daymap.jpg'
 import EarthNormalMap from '../../../../assets/8k_earth_normal_map.jpg'
@@ -7,18 +7,20 @@ import EarthSpecularMap from '../../../../assets/8k_earth_specular_map.jpg'
 import EarthCloudsMap from '../../../../assets/8k_earth_clouds.jpg'
 import { useFrame, useLoader, useThree } from '@react-three/fiber'
 import { Mesh, TextureLoader } from 'three'
+import Loader from '../../../Loader/Loader'
 interface Props {
     latitude: number;
     longitude: number;
+    handleGlobeLoaded:()=>void;
 }
-const Earth: React.FC<Props> = ({ latitude, longitude }) => {
+const Earth: React.FC<Props> = ({ latitude, longitude,handleGlobeLoaded }) => {
     const earthRef = useRef<Mesh>(null)
     const cloudsRef = useRef<Mesh>(null)
     const markerRef = useRef<Mesh>(null);
     const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap])
 
     const earthRadius = 1;
-    const [rotationStopped, setRotationStopped] = useState(false);
+    //const [rotationStopped, setRotationStopped] = useState(false);
     const xRef = useRef(0);
     const yRef = useRef(0);
     const zRef = useRef(0);
@@ -29,51 +31,49 @@ const Earth: React.FC<Props> = ({ latitude, longitude }) => {
         xRef.current = Math.cos(latRad) * Math.cos(lonRad) * earthRadius;
         yRef.current = Math.sin(latRad) * earthRadius;
         zRef.current = Math.cos(latRad) * Math.sin(lonRad) * earthRadius;
+        handleGlobeLoaded()
     }, [latitude, longitude]);
 
     useFrame(({ clock }) => {
-        if (!rotationStopped) {
-            const elapsedTime = clock.getElapsedTime();
+        const elapsedTime = clock.getElapsedTime();
 
-            if (earthRef.current && cloudsRef.current) {
-                earthRef.current.rotation.y = elapsedTime / 3; 
-                cloudsRef.current.rotation.y = elapsedTime / 3; 
-            }
-            if (markerRef.current) {
-                markerRef.current.position.set(xRef.current, yRef.current, zRef.current);
-            }
-
+        if (earthRef.current && cloudsRef.current) {
+            earthRef.current.rotation.y = elapsedTime / 3;
+            cloudsRef.current.rotation.y = elapsedTime / 3;
+        }
+        if (markerRef.current) {
+            markerRef.current.position.set(xRef.current, yRef.current, zRef.current);
         }
     })
 
     return (
         <>
             <ambientLight intensity={5} />
-            {/* <pointLight color="#f6f3ea" position={[2,0,3]} intensity={10}/> */}
-            <Stars radius={300} depth={60} count={1000} factor={7} saturation={0} fade={true} />
-            <mesh ref={cloudsRef} position={[0, 0, 3]}>
-                <sphereGeometry args={[1.005, 32, 32]} />
-                <meshPhongMaterial
-                    map={cloudsMap}
-                    opacity={0.4}
-                    depthWrite={true}
-                    transparent={true}
-                    side={THREE.DoubleSide} />
-            </mesh>
-            <mesh ref={earthRef} position={[0, 0, 3]}>
-                <sphereGeometry args={[1, 32, 32]} />
-                <meshPhongMaterial specularMap={specularMap} />
-                <meshStandardMaterial
-                    map={colorMap}
-                    normalMap={normalMap}
-                    metalness={0.4}
-                    roughness={0.7} />
-                <mesh ref={markerRef}>
-                    <sphereGeometry args={[0.015, 32, 32]} />
-                    <meshBasicMaterial color="red" />
+                {/* <pointLight color="#f6f3ea" position={[2,0,3]} intensity={10}/> */}
+                <Stars radius={300} depth={60} count={1000} factor={7} saturation={0} fade={true} />
+                <mesh ref={cloudsRef} position={[0, 0, 3]}>
+                    <sphereGeometry args={[1.005, 32, 32]} />
+                    <meshPhongMaterial
+                        map={cloudsMap}
+                        opacity={0.4}
+                        depthWrite={true}
+                        transparent={true}
+                        side={THREE.DoubleSide} />
                 </mesh>
-                {/* <OrbitControls /> */}
-            </mesh>
+                <mesh ref={earthRef} position={[0, 0, 3]}>
+                    <sphereGeometry args={[1, 32, 32]} />
+                    <meshPhongMaterial specularMap={specularMap} />
+                    <meshStandardMaterial
+                        map={colorMap}
+                        normalMap={normalMap}
+                        metalness={0.4}
+                        roughness={0.7} />
+                    <mesh ref={markerRef}>
+                        <sphereGeometry args={[0.015, 32, 32]} />
+                        <meshBasicMaterial color="red" />
+                    </mesh>
+                    {/* <OrbitControls /> */}
+                </mesh>
         </>
     )
 }
